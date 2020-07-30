ffe7f355b28488bd7d621825350037f12c2a2286
MatheusNobre <matheus.gomes@serget.com.br>
2020-07-30 14:40:06 -0300

    Creating a index

---
+++ velhia.web/src/views/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <div class="buttons">
+      <b-button type="is-primary" expanded rounded>Player vs Player</b-button>
+      <b-button type="is-warning" expanded rounded>Player vs S.A</b-button>
+      <b-button type="is-info" expanded rounded>Player vs A.I</b-button>
+      <b-button type="is-danger" expanded rounded>S.A vs A.I</b-button>
+    </div>
+  </div>
+</template>
+<script>
+</script>
