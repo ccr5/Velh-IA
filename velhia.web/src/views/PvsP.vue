4b02f6899cfeaf012fdeaad639eb9576f86352aa
MatheusNobre <matheus.gomes@serget.com.br>
2020-08-05 15:54:32 -0300

    Changing PvsP screen

--- velhia.web/src/views/PvsP.vue
+++ velhia.web/src/views/PvsP.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="hero is-light is-bold is-fullheight">
     <div class="hero-body">
-      <div class="columns info-historic">
-        <div class="#">
+      <div class="columns is-12">
+        <div class="column info-historic">
           <div class="box">
             <h1 class="title">Historic Matchs</h1>
             <b-table :data="historic_datas" :columns="historic_datas_columns"></b-table>
@@ -12,45 +12,26 @@
             <b-table :data="match_datas" :columns="match_data_columns"></b-table>
           </div>
         </div>
-      </div>
-      <div class="columns info-match">
-        <div class="columns">
+        <div class="column"></div>
+        <div class="column info-match">
           <div class="Col">
-            <div class="Campo C1L1">
-              C1L1
-            </div>
-            <div class="Campo C1L2">
-              C1L2
-            </div>
-            <div class="Campo C1L3">
-              C1L3
-            </div>
+            <div class="Campo C1L1">C1L1</div>
+            <div class="Campo C1L2">C1L2</div>
+            <div class="Campo C1L3">C1L3</div>
           </div>
         </div>
-        <div class="columns">
+        <div class="column info-match">
           <div class="Col">
-            <div class="Campo C2L1">
-              C2L1
-            </div>
-            <div class="Campo C2L2">
-              C2L2
-            </div>
-            <div class="Campo C2L3">
-              C2L3
-            </div>
+            <div class="Campo C2L1">C2L1</div>
+            <div class="Campo C2L2">C2L2</div>
+            <div class="Campo C2L3">C2L3</div>
           </div>
         </div>
-        <div class="columns">
+        <div class="column info-match">
           <div class="Col">
-            <div class="Campo C3L1">
-              C3L1
-            </div>
-            <div class="Campo C3L2">
-              C3L2
-            </div>
-            <div class="Campo C3L3">
-              C3L3
-            </div>
+            <div class="Campo C3L1">C3L1</div>
+            <div class="Campo C3L2">C3L2</div>
+            <div class="Campo C3L3">C3L3</div>
           </div>
         </div>
       </div>
@@ -58,108 +39,111 @@
   </section>
 </template>
 <script>
-import $ from 'jquery'
-import axios from 'axios'
+import $ from "jquery";
+import axios from "axios";
 export default {
   data() {
     return {
       historic_datas: [],
       historic_datas_columns: [
         {
-          field: 'player',
-          label: 'Player',
+          field: "player",
+          label: "Player"
         },
         {
-          field: 'total',
-          label: 'Total',
+          field: "total",
+          label: "Total",
           centered: true
         },
         {
-          field: 'wins',
-          label: 'Wins',
+          field: "wins",
+          label: "Wins",
           centered: true
         },
         {
-          field: 'draws',
-          label: 'Draws',
+          field: "draws",
+          label: "Draws",
           centered: true
         },
         {
-          field: 'losses',
-          label: 'Losses',
+          field: "losses",
+          label: "Losses",
           centered: true
         }
       ],
       match_data: [],
       match_data_columns: [
         {
-          field: 'player',
-          label: 'Player',
+          field: "player",
+          label: "Player"
         },
         {
-          field: 'total',
-          label: 'Total',
+          field: "total",
+          label: "Total",
           centered: true
         },
         {
-          field: 'wins',
-          label: 'Wins',
+          field: "wins",
+          label: "Wins",
           centered: true
         },
         {
-          field: 'draws',
-          label: 'Draws',
+          field: "draws",
+          label: "Draws",
           centered: true
         },
         {
-          field: 'losses',
-          label: 'Losses',
+          field: "losses",
+          label: "Losses",
           centered: true
         }
       ]
-    }
+    };
   },
   methods: {
-    getHistoricData () {
-      axios.get('http://localhost:5000/api/cliente').then(ret => {
-        this.historic_datas = ret.data
-      })
+    getHistoricData() {
+      axios.get("http://localhost:5000/api/cliente").then(ret => {
+        this.historic_datas = ret.data;
+      });
     }
   },
-  mounted () {
-    $(document).ready(function(){
+  mounted() {
+    $(document).ready(function() {
       var Altura = $(window).height();
+      var Largura = $(window).width();
       $(".info-historic").height(Altura);
       $(".info-match").height(Altura);
-      $(".C1L1").on("click", function (){
-        alert("C1L1")
+      $(".Col").width(Largura / 5);
+      $(".Campo").height(Altura / 4);
+      $(".C1L1").on("click", function() {
+        alert("C1L1");
       });
-      $(".C1L2").on("click", function (){
-        alert("C1L2")
+      $(".C1L2").on("click", function() {
+        alert("C1L2");
       });
-      $(".C1L3").on("click", function (){
-        alert("C1L3")
+      $(".C1L3").on("click", function() {
+        alert("C1L3");
       });
-      $(".C2L1").on("click", function (){
-        alert("C2L1")
+      $(".C2L1").on("click", function() {
+        alert("C2L1");
       });
-      $(".C2L2").on("click", function (){
-        alert("C2L2")
+      $(".C2L2").on("click", function() {
+        alert("C2L2");
       });
-      $(".C2L3").on("click", function (){
-        alert("C2L3")
+      $(".C2L3").on("click", function() {
+        alert("C2L3");
       });
-      $(".C3L1").on("click", function (){
-        alert("C3L1")
+      $(".C3L1").on("click", function() {
+        alert("C3L1");
       });
-      $(".C3L2").on("click", function (){
-        alert("C3L2")
+      $(".C3L2").on("click", function() {
+        alert("C3L2");
       });
-      $(".C3L3").on("click", function (){
-        alert("C3L3")
+      $(".C3L3").on("click", function() {
+        alert("C3L3");
       });
     });
-    this.getHistoricData()
+    this.getHistoricData();
   }
 };
 </script>
@@ -169,13 +153,6 @@ export default {
 }
 .info-match {
   margin-top: 20px;
-  margin-left: 10%;
-}
-.Col {
-  min-width: 250px;
-}
-.Campo {
-  min-height: 150px;
 }
 .C1L2 {
   border-top-style: solid;
