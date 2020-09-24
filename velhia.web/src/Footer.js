63e92cfd0fb539c8db52468cc13c67ed8cb719f7
matt-gomes <matt-gomes@live.com>
2020-09-24 19:06:41 -0300

    creating Header and Footer

---
+++ velhia.web/src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Footer() {
+	return (
+		<footer className="velhia-footer">
+			footer
+		</footer>
+	)
+}
+
+export default Footer
---
+++ velhia.web/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+	return (
+		<nav className="navbar navbar-light bg-light">
+			<a class="navbar-brand" href="/">Velh-IA</a>
+		</nav>
+	)
+}
+
+export default Header
