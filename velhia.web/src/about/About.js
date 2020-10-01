39ad6a4b5da2f8c4af2dd4a6a6ae539ec638f430
Matheus N <Matheus N>
2020-09-30 21:13:37 -0300

    Editing about page

--- velhia.web/src/about/About.js
+++ velhia.web/src/about/About.js
@@ -11,16 +11,18 @@ function About() {
       <Header />
       <div className="container-fluid" style={{ minHeight: window.innerHeight - 110 }}>
         <div className="jumbotron field">
-          <h1 className="display-4">So what?</h1>
+          <h1 className="display-4">Introduction</h1>
           <p className="lead">
-            Velh-IA is a part of a scientific research called <strong> Artificial
+            Velh-IA is environment of a scientific research called <strong> Artificial
             Collective consciousness - Analysis of the Interactions between Artificial Intelligences</strong>
-            . The objective is analyze the interactions, behaviors and results of a multi-agent system with no objective,
-            under an uncontrolled and unknown environment, applying sociological concepts. Okay, let's start again?
-            Behind this website, two players are playing a Tic Tac Toe game: Statistical Algorithm and AI Network.
+            . The objective is analyze the interactions, behaviors and results of a multi-agent system with no goal,
+            under an uncontrolled and unknown environment, applying sociological concepts.
+          </p>
+          <p className="lead">
+            Behind this website, two players are competing a Tic Tac Toe game: Statistical Algorithm and AI Network.
           </p>
           <div className="row" style={{ marginTop: "30px" }}>
-            <div className="col-6">
+            <div className="col-6" id="SA">
               <h3 className="display-5" style={{ textAlign: "center" }}> Statistical Algorithm (SA) </h3>
               <p className="lead">
                 Algorithm programmed based on all the rules and objectives of the
@@ -31,12 +33,14 @@ function About() {
               <h3 className="display-5" style={{ textAlign: "center" }}> Multi-Agent System (MAS) </h3>
               <p className="lead">
                 Network with AI organizations interacting and sharing informations, where a
-                representative agent plays based on previous experiences of the agents in the network (stored memory).
+                representative agent plays based on previous experiences of the agents in the network (stored memory)
               </p>
             </div>
           </div>
           <p className="lead">
-            The video below presents the whole concept in a simple and objective way. We suggest that you watch
+            The MAS is the key of whole research. He was programmed applying Social Fact and
+            functionalism theory to simulate our human relations. The video below presents the whole
+            concept in a simple and objective way. We suggest that you watch
             before you move on to more specific approaches.
           </p>
           <iframe title="Pitch" width={window.innerWidth * 0.8} height={window.innerHeight * 0.8}
@@ -46,7 +50,7 @@ function About() {
       </div>
       <Footer />
     </div >
-  );
+  )
 }
 
 export default About
