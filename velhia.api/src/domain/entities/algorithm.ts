7f8b7c1fd9dbfa6584f985eeb6d93f68ce538c63
matt-gomes <matt-gomes@live.com>
2020-12-29 20:43:46 -0300

    Velhia.api Ok

--- velhia.api/src/domain/entities/algorithm.ts
+++ velhia.api/src/domain/entities/algorithm.ts
@@ -5,20 +5,6 @@ import { environmentReaction } from '@enums/environmentReaction'
 const AlgorithmSchema: Schema = new Schema<IAlgorithm>(
   {
     birth: { type: Date, required: true },
-    memory: [{
-      matchId: { type: String, required: true },
-      isLearner: { type: Boolean, required: true },
-      choices: [{
-        dateRequest: { type: Date, required: true },
-        gameStatus: { type: Array, required: true },
-        timeToAct: { type: Number, required: true },
-        action: { type: Number, required: true }
-      }],
-      environmentReaction: {
-        type: String,
-        enum: Object.values(environmentReaction)
-      }
-    }],
     matchs: { type: Number, required: true },
     victories: { type: Number, required: true },
     defeats: { type: Number, required: true },
