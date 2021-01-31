4ec152ab75bb082d8f27bc690ab8cbc18e5fd721
matt-gomes <matt-gomes@live.com>
2021-01-31 14:25:33 -0300

    Starting to create automated test in velhia.game

--- velhia.game/tests/test_velhia.py
+++ velhia.game/tests/test_velhia.py
@@ -1,6 +1,23 @@
 from velhia import Velhia
+from config.database import Database
+
 
 class TestVelhia:
+
+    match_test_db = Database('http://localhost:3000/',
+                             'v1', 'matchtests')
+    family_test_db = Database('http://localhost:3000/',
+                              'v1', 'familytests')
+    algorithm_test_db = Database('http://localhost:3000/',
+                                 'v1', 'algorithmtests')
+    education_test_db = Database('http://localhost:3000/',
+                                 'v1', 'educationtests')
+    religion_test_db = Database('http://localhost:3000/',
+                                'v1', 'religiontests')
+
+    velhia = Velhia(match_test_db, family_test_db,
+                    education_test_db, religion_test_db,
+                    algorithm_test_db)
 
     def test_play(self):
         sa = StatisticalAlgorithm({}, ('X', 1), ('O', 0))
