530b21bc8efd4cdf9e19871da48bbaedcf0aad9b
matt-gomes <matt-gomes@live.com>
2021-01-31 19:00:10 -0300

    some updates in database test

--- velhia.game/tests/test_database.py
+++ velhia.game/tests/test_database.py
@@ -4,8 +4,7 @@ import json
 
 class TestDatabase:
 
-    db = Database('http://localhost:3000/api/', 'v1', 'algorithms')
-    obj_id = ''
+    db = Database('http://localhost:3000/test/', 'v1', 'algorithms')
     obj = {"birth": "Thu Jan 05 2017 22:12:46 GMT-0100 (CET)",
            "matchs": 1,
            "victories": 0,
@@ -14,20 +13,46 @@ class TestDatabase:
 
     def test_create(self):
         alg = self.db.create(json.dumps(self.obj))
-        self.obj_id = alg.json()['_id']
         assert alg.status_code == 200
+        assert alg.json()['matchs'] == self.obj['matchs']
+        assert alg.json()['victories'] == self.obj['victories']
+        assert alg.json()['defeats'] == self.obj['defeats']
+        assert alg.json()['draw'] == self.obj['draw']
 
     def test_get(self):
-        alg = self.db.create(json.dumps(self.obj))
-        self.obj_id = alg.json()['_id']
-        alg = self.db.get(filters={"_id": self.obj_id})
+        alg = self.db.get(filters={"matchs": self.obj['matchs']})
         assert alg.status_code == 200
+        assert alg.json()[0]['matchs'] == self.obj['matchs']
+        assert alg.json()[0]['victories'] == self.obj['victories']
+        assert alg.json()[0]['defeats'] == self.obj['defeats']
+        assert alg.json()[0]['draw'] == self.obj['draw']
         alg = self.db.get(offset=0, limit=1)
         assert alg.text != '[]'
+        assert alg.status_code == 200
+        assert alg.json()[0]['matchs'] == self.obj['matchs']
+        assert alg.json()[0]['victories'] == self.obj['victories']
+        assert alg.json()[0]['defeats'] == self.obj['defeats']
+        assert alg.json()[0]['draw'] == self.obj['draw']
 
     def test_update(self):
-        alg = self.db.create(json.dumps(self.obj))
-        self.obj_id = alg.json()['_id']
-        self.obj = alg.json()
-        alg = self.db.update(object_id=self.obj_id, obj=json.dumps(self.obj))
+        alg = self.db.get(filters={"matchs": self.obj['matchs']})
+        obj_id = alg.json()[0]['_id']
+        alg = self.db.update(object_id=obj_id,
+                             obj=json.dumps(self.obj))
         assert alg.status_code == 200
+        assert alg.json()['matchs'] == self.obj['matchs']
+        assert alg.json()['victories'] == self.obj['victories']
+        assert alg.json()['defeats'] == self.obj['defeats']
+        assert alg.json()['draw'] == self.obj['draw']
+
+    def test_delete(self):
+        alg = self.db.get(filters={"matchs": self.obj['matchs']})
+        alg = self.db.delete(object_id=alg.json()[0]['_id'])
+        assert alg.status_code == 200
+        assert alg.json()['matchs'] == self.obj['matchs']
+        assert alg.json()['victories'] == self.obj['victories']
+        assert alg.json()['defeats'] == self.obj['defeats']
+        assert alg.json()['draw'] == self.obj['draw']
+        alg = self.db.get(offset=0, limit=1)
+        assert alg.status_code == 200
+        assert alg.text == '[]'
